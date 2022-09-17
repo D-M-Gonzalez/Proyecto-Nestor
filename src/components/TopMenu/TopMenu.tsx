c13b728908e3067a53576394f64ef34533521d28
Armando Rivas <armandoweb.agency@gmail.com>
2022-09-17 14:24:11 -0300

    agregado logo formato svg

--- src/components/TopMenu/TopMenu.tsx
+++ src/components/TopMenu/TopMenu.tsx
@@ -13,6 +13,8 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
+import logo from '../../assets/logo-dark.svg'
+
 //import Navigate from '../../routes/Navigator';
 
 
@@ -64,7 +66,7 @@ export const TopMenu = () => {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Box component="img" sx={{ width: 220, display: { xs: 'none', md: 'flex' } }} src={logo} />
           <Typography
             variant="h6"
             noWrap
@@ -80,7 +82,6 @@ export const TopMenu = () => {
               textDecoration: 'none',
             }}
           >
-            LOGO
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
@@ -114,12 +115,12 @@ export const TopMenu = () => {
             >
               {pages.map((page) => (
                 <MenuItem key={page.title} onClick={handleCloseNavMenu}>
-                  <Link to={page.title} style={{ textDecoration: "none" }}><Typography textAlign="center">{page.title}</Typography></Link>
+                  <Link to={page.title} style={{ textDecoration: "none", color: "#000" }}><Typography textAlign="center">{page.title}</Typography></Link>
                 </MenuItem>
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Box component="img" sx={{ width: 220, display: { xs: 'flex', md: 'none' } }} src={logo} />
           <Typography
             variant="h5"
             noWrap
@@ -136,15 +137,14 @@ export const TopMenu = () => {
               textDecoration: 'none',
             }}
           >
-            LOGO
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, justifyContent: 'right', display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
-              <Link to={page.url}>
+              <Link to={page.url} key={page.title} style={{ textDecoration: "none", color: "#fff" }}>
                 <Button
-                  key={page.title}
                   onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: 'white', display: 'block' }}
+                  sx={{ my: 2, mx: 2, color: 'white', display: 'block' }}
+                  variant="contained"
                 >
                   {page.title}
                 </Button>
@@ -152,36 +152,6 @@ export const TopMenu = () => {
               </Link>
 
             ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
           </Box>
         </Toolbar>
       </Container>
