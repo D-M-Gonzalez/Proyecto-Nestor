afb6111418e8c64f7742a19c89e1644dd344ac24
Armando Rivas <armandoweb.agency@gmail.com>
2022-09-23 18:32:36 -0300

    creacion seccion servicios, cards e info en los json

---
+++ src/components/Cards/ServiceCard.tsx
@@ -0,0 +1,53 @@
+import { Box, Container, ImageList, Paper, Typography, Grid } from '@mui/material'
+import React from 'react'
+import styles from './Logo.module.css';
+
+export interface CardProps {
+  title: string
+  text_1: string
+  text_2: string
+  text_3: string
+  image: string
+}
+
+export default function ServiceCard(props: CardProps) {
+  const { title, text_1, text_2, text_3, image } = props
+
+  return (
+    <>
+
+
+
+      <Box display="flex" m={10}>
+        <Grid container mt={10}>
+          <Box mt={5}>
+            <Box
+              component='img'
+              src={image}
+              borderRadius="20px"
+              width={{ md: 523, xs: 150 }}
+            />
+          </Box>
+
+          <Grid item xs={0.5} />
+          <Grid item container xs={6}>
+            <Typography fontFamily='IntegralCF' color='white' fontSize={{ xl: 70, lg: 40, md: 30, xs: 20 }} fontWeight={400} lineHeight={1.5}>
+              {title}
+            </Typography>
+            <Typography fontFamily='Nunito Sans' color='white' mt={5} fontSize={{ xl: 30, lg: 20, md: 15, xs: 10 }} fontWeight={400} lineHeight={1.2}>
+              {text_1}
+            </Typography>
+            <Typography fontFamily='Nunito Sans' color='white' mt={5} fontSize={{ xl: 30, lg: 20, md: 15, xs: 10 }} fontWeight={400} lineHeight={1.2}>
+              {text_2}
+            </Typography>
+            <Typography fontFamily='Nunito Sans' color='white' mt={5} fontSize={{ xl: 30, lg: 20, md: 15, xs: 10 }} fontWeight={400} lineHeight={1.2}>
+              {text_3}
+            </Typography>
+          </Grid>
+          <Grid item xs={5.5} />
+        </Grid>
+      </Box>
+    </>
+
+  )
+}
--- src/containers/Services/Services.tsx
+++ src/containers/Services/Services.tsx
@@ -1,6 +1,16 @@
 import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import ServiceCard from '../../components/Cards/ServiceCard';
 import { useImageMultiplier } from '../../store/Main';
+import { Box, Button, Grid, Typography } from '@mui/material';
+import ServiceHero from '../../assets/services-hero.png';
+
+const background = {
+    width: "auto",
+    height: "50vw",
+    backgroundImage: `url(${ServiceHero})`,
+    backgroundSize: 'cover',
+}
 
 export interface IServicesProps { }
 
@@ -12,5 +22,70 @@ export default function Services(props: IServicesProps) {
         console.log(imageSizeMultiplier)
     })
 
-    return <div>Services</div>;
+    const CardList = [
+        {
+            id: 'implementation',
+            title: t('services.cards.implementation.title'),
+            text_1: t('services.cards.implementation.text_1'),
+            text_2: t('services.cards.implementation.text_2'),
+            text_3: t('services.cards.implementation.text_3'),
+            image: '/assets/automatizacion-infraestructura-cloud.png'
+        },
+        {
+            id: 'advice',
+            title: t('services.cards.advice.title'),
+            text_1: t('services.cards.advice.text_1'),
+            text_2: t('services.cards.advice.text_2'),
+            text_3: t('services.cards.advice.text_3'),
+            image: '/assets/asesoramiento-ciberseguridad.png'
+        },
+        {
+            id: 'outsourcing',
+            title: t('services.cards.outsourcing.title'),
+            text_1: t('services.cards.outsourcing.text_1'),
+            text_2: t('services.cards.outsourcing.text_2'),
+            text_3: t('services.cards.outsourcing.text_3'),
+            image: '/assets/outsourcing.png'
+        },
+        {
+            id: 'recruitment',
+            title: t('services.cards.recruitment.title'),
+            text_1: t('services.cards.recruitment.text_1'),
+            text_2: t('services.cards.recruitment.text_2'),
+            text_3: t('services.cards.recruitment.text_3'),
+            image: '/assets/recruitment.png'
+        },
+
+    ]
+
+    return (
+        <>
+            <Box style={background}>
+                <Box display="flex" mt={10}>
+                    <Grid container mt={15}>
+                        <Grid item xs={0.5} />
+                        <Grid item container xs={5}>
+                            <Typography fontFamily='IntegralCF' color='white' fontSize={{ xl: 70, lg: 40, md: 30, xs: 20 }} fontWeight={400} lineHeight={1.5}>
+                                {t('services.main.services_title')}
+                            </Typography>
+                            <Typography fontFamily='Nunito Sans' color='white' mt={5} fontSize={{ xl: 30, lg: 20, md: 15, xs: 10 }} fontWeight={400} lineHeight={1.2}>
+                                {t('services.main.services_text_1')}
+                            </Typography>
+                            <Typography fontFamily='Nunito Sans' color='white' mt={5} fontSize={{ xl: 30, lg: 20, md: 15, xs: 10 }} fontWeight={400} lineHeight={1.2}>
+                                {t('services.main.services_text_2')}
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Box>
+
+            </Box>
+            <Box mt={0}>
+                {CardList.map((card) => (<ServiceCard key={card.id} title={card.title} text_1={card.text_1} text_2={card.text_2} text_3={card.text_3} image={card.image} />))}
+            </Box>
+
+
+
+
+        </>
+    );
 }
